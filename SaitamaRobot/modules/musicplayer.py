f873754fe04926f4358aa761afb35feda19df27d
animelover-devil <78991109+animelover-devil@users.noreply.github.com>
2021-04-23 06:26:47 +0530

    Update musicplayer.py

--- SaitamaRobot/modules/musicplayer.py
+++ SaitamaRobot/modules/musicplayer.py
@@ -4,7 +4,7 @@ MUSIC PLAYER
 Music player which helps you to plays music in your group's voice chat
 
  Setting up
-1) Add @JARVISMUSICASSISTANT to your group
+1) Add @miko_music_bot and @superior_demon to your group
 2) Make bot admin
 3) Start a voice chat
 
@@ -13,7 +13,8 @@ Music player which helps you to plays music in your group's voice chat
 - /pause : Pause music play (Admin only)
 - /skip : Skips current song (Admin only)
 - /end : End the music play (Admin only)
--/search : To search a particular song on youtube
+-Tag bot to search from youtube and get link .
+EX. @miko_music_bot <song name> then bot will show song info through inline mode just click on it , bot will send link then reply that link with /play and enjoy .
 
 
 
